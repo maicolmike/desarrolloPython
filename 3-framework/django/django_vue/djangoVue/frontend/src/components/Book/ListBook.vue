dcbd791dedaa0000137a4559abaf26e91d258411
Maicol <maicolmike@hotmail.com>
2024-09-05 17:28:53 -0500

    framework

---
+++ 3-framework/django/django_vue/djangoVue/frontend/src/components/Book/ListBook.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div class="container">
+      <div class="row">
+        <div class="col text-left">
+          <h2>Listado de libros</h2>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ListBook',
+  };
+  </script>
+  
+  <style lang="css" scoped>
+  /* Aquí puedes agregar estilos específicos del componente */
+  </style>
+  
