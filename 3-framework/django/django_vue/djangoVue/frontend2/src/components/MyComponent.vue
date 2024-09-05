40f3bcec97f707195b356890a0402a2646505941
Maicol <maicolmike@hotmail.com>
2024-09-05 10:50:31 -0500

    framework

---
+++ 3-framework/django/django_vue/djangoVue/frontend2/src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h2>{{ title }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyComponent',
+    data() {
+      return {
+        title: 'Hola desde MyComponent',
+        message: 'Este es un componente en Vue 3.'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  h2 {
+    color: blue;
+  }
+  </style>
+  
