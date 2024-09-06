9f20a4cf639fe52dda730f0950934eb73ab43a34
Maicol <maicolmike@hotmail.com>
2024-09-06 17:27:05 -0500

    framework vue

--- 3-framework/django/django_vue/djangoVue/frontend/src/components/Book/ListBook.vue
+++ 3-framework/django/django_vue/djangoVue/frontend/src/components/Book/ListBook.vue
@@ -3,6 +3,15 @@
       <div class="row">
         <div class="col text-left">
           <h2>Listado de libros</h2>
+
+          <div class="col-md-12">
+
+          <b-table striped hover :items="books" :fields="fields">
+
+          </b-table>
+
+          </div>
+
         </div>
       </div>
     </div>
@@ -10,6 +19,16 @@
   
   <script>
   export default {
+    data () {
+    return {
+      fields: [
+        { key: 'title', label: 'Título' },
+        { key: 'description', label: 'Descripción' },
+        { key: 'action', label: 'Action' }
+      ],
+      books: []
+    }
+  },
    
   };
   </script>
