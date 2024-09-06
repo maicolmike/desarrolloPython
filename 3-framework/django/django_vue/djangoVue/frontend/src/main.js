ebf7ba47d1f3fccbf1634245d0f5ea91eb4e6331
Maicol <maicolmike@hotmail.com>
2024-09-06 14:38:01 -0500

    framework vue

--- 3-framework/django/django_vue/djangoVue/frontend/src/main.js
+++ 3-framework/django/django_vue/djangoVue/frontend/src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//import { createApp } from 'vue'
+//import App from './App.vue'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+// main.js (o main.ts para TypeScript)
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// Import Bootstrap and BootstrapVue CSS files
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+// Opcional: Importar los iconos de Bootstrap
+//import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Importar BootstrapVue 3
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+const app = createApp(App);
+
+// Usar BootstrapVue 3
+app.use(BootstrapVue3);
+
+app.mount('#app');
+
