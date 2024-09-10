fc40d077ff0d9a59c5409403089cf6535d198709
Maicol <maicolmike@hotmail.com>
2024-09-10 11:44:57 -0500

    framework vue

--- 3-framework/django/django_vue/djangoVue/frontend/src/components/Book/ListBook.vue
+++ 3-framework/django/django_vue/djangoVue/frontend/src/components/Book/ListBook.vue
@@ -1,57 +1,62 @@
-<template lang="">
-    <div class="container">
-      <div class="row">
-        <div class="col text-left">
-          <h2>Listado de libros</h2>
-
-          <div class="col-md-12">
-
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col text-left">
+        <h2>Listado de libros</h2>
+        <div class="col-md-12">
+          <!-- Definir la tabla con slots y campos -->
           <b-table striped hover :items="books" :fields="fields">
+            
+            <!-- Slot para las acciones de cada fila -->
+            <template v-slot:cell(action)>
+              <!-- Agregar la clase mr-2 para dar margen a la derecha -->
+              <b-button size="sm" variant="primary" class="" style="margin-right: 10px;">
+                Editar
+              </b-button>
+              <b-button size="sm" variant="danger">
+                Eliminar
+              </b-button>
+            </template>
 
           </b-table>
-
-          </div>
-
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
+  </div>
+</template>
 
-  export default {
-    data () {
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
     return {
       fields: [
         { key: 'title', label: 'Título' },
         { key: 'description', label: 'Descripción' },
-        { key: 'action', label: 'Action' }
+        { key: 'action', label: 'Acciones' }
       ],
       books: []
     }
   },
-
   methods: {
-
-  getBooks () {
-    const path = 'http://localhost:8000/api/v1.0/books/'
-    axios.get(path).then((response) => {
-      this.books = response.data
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
-},
-created(){
+    getBooks () {
+      const path = 'http://localhost:8000/api/v1.0/books/'
+      axios.get(path)
+        .then((response) => {
+          this.books = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  },
+  created() {
     this.getBooks()
   }
-   
-  };
-  </script>
-  
-  <style lang="css" scoped>
-  /* Aquí puedes agregar estilos específicos del componente */
-  </style>
-  
+}
+</script>
+
+<style lang="css" scoped>
+/* Estilos específicos del componente */
+</style>
